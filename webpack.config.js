1fcc87dfacf571e2b97ddd0c16cd4f1a19735d36
waysaku <waysaku@gmail.com>
2020-05-31 18:31:34 +0900

    reduce duplicated webpack execution

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
   entry: {
-    td: './lib/index.js'
+    'td': './lib/index.js',
+    'td.min': './lib/index.js'
   },
   output: {
     filename: '[name].js',
@@ -14,26 +15,28 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
     }),
-    process.env.MINIFY_BUILD
-      ? new UglifyJSPlugin({
-        compress: {
-          screw_ie8: false
-        },
-        mangle: {
-          screw_ie8: false
-        },
-        output: {
-          screw_ie8: false,
-          quote_keys: true
-        }
-      })
-      : new UglifyJSPlugin({
-        compress: false,
-        mangle: false,
-        beautify: true,
-        output: {
-          quote_keys: true
-        }
-      })
+    new UglifyJSPlugin({
+      compress: false,
+      mangle: false,
+      beautify: true,
+      output: {
+        quote_keys: true
+      },
+      include: /td\.js$/
+    }),
+    new UglifyJSPlugin({
+      compress: {
+        screw_ie8: false
+      },
+      mangle: {
+        screw_ie8: false
+      },
+      output: {
+        screw_ie8: false,
+        quote_keys: true
+      },
+      include: /td\.min\.js$/,
+      minimize: true
+    })
   ]
 }
